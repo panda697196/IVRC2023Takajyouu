929fb8c6a478cee9232a3d93f8cf6843445a3db8
Ryo-o-913 <66991401+Ryo-o-913@users.noreply.github.com>
2023-08-30 16:21:43 +0900

    Modify HardwareManagerCode（Debug））
    デバッグ用コード大幅追記

--- Assets/Scripts/Hardware/HardwareManager.cs
+++ Assets/Scripts/Hardware/HardwareManager.cs
@@ -23,7 +23,15 @@ public class HardwareManager : MonoBehaviour
     [SerializeField] private float _weightTimeR = 5;
     [Header("デバック等")]
     public bool _isDebug = true;
-    
+    [SerializeField] private float _weightPosition = 0;//おもりの位置　上が0で下げるほど＋
+    [SerializeField] private float _ropePosition = 0; //ロープの位置　たるんでいると0できつくするほど+
+
+    private bool _isWeightUp = false; // おもりが動いているかどうか
+    private bool _isWeightDown = false;
+    private bool _isRopeTight = false; // ロープが動いているがどうか
+    private bool _isRopeLoose = false;
+
+    private float _timeToUpdate;
 
     // Start is called before the first frame update
     void Start()
@@ -34,24 +42,62 @@ public class HardwareManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //おもりの場所を把握（単位は[sec]）
+        _timeToUpdate = Time.deltaTime;
+        if (_isWeightUp)
+            _weightPosition -= _timeToUpdate;
+        if (_isWeightDown)
+            _weightPosition += _timeToUpdate;
+        if (_isRopeTight)
+            _ropePosition += _timeToUpdate;
+        if (_isRopeLoose)
+            _ropePosition -= _timeToUpdate;
+        
+        
         //Debug
         if (_isDebug)
         {
-            if (Input.GetKey(KeyCode.O))
+            if (Input.GetKey(KeyCode.P))
             {
                 StartCoroutine(Appear());
             }
-            if (Input.GetKey(KeyCode.K))
+            if (Input.GetKey(KeyCode.L))
             {
                 StartCoroutine(Disappear());
             }
-            if (Input.GetKey(KeyCode.P))
+            
+            if (Input.GetKeyDown(KeyCode.I))
             {
-                // おもり　StartCoroutine(Disappear());
+                _ropeSender.DataSend("C\n" + _ropeSpeedC.ToString() + "\n"); // 紐を張る
             }
-            if (Input.GetKey(KeyCode.L))
+            if (Input.GetKeyUp(KeyCode.I))
             {
-                // おもり　StartCoroutine(Disappear());
+                _ropeSender.DataSend("S\n");
+            }
+
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                _ropeSender.DataSend("R\n" + _ropeSpeedR.ToString() + "\n"); // 紐を緩める
+            }
+            if (Input.GetKeyUp(KeyCode.J))
+            {
+                _ropeSender.DataSend("S\n");
+            }
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                _weightSender.DataSend("R\n" + _weightSpeedR.ToString() + "\n");
+            }
+            if (Input.GetKeyUp(KeyCode.U))
+            {
+                _weightSender.DataSend("S\n");
+            }
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                _weightSender.DataSend("C\n" + _weightSpeedC.ToString() + "\n"); // 重りを落とし始める
+            }
+            if (Input.GetKeyUp(KeyCode.H))
+            {
+                _weightSender.DataSend("S\n");
             }
         }
     }
@@ -85,20 +131,30 @@ public class HardwareManager : MonoBehaviour
     public IEnumerator AppearShock() // 鷹が腕に止まるときの衝撃提示の関数
     {
         _ropeSender.DataSend("C\n" + _ropeSpeedC.ToString() + "\n"); // 紐を張る
+        _isRopeTight = true;
         yield return new WaitUntil(() => _ropeSender._afterstop == true); // 紐が張ったことを確認できるまで待機
+        _isRopeTight = false;
+        
         _weightSender.DataSend("C\n" + _weightSpeedC.ToString() + "\n"); // 重りを落とし始める
+        _isWeightDown = true;
         yield return new WaitForSeconds(_weightTimeC);
         _weightSender.DataSend("S\n"); // 重りが落ちたらモータを止める
+        _isWeightDown = false;
+        
         _ropeSender.DataSend("R\n" + _ropeSpeedR.ToString() + "\n"); // 紐を緩める
+        _isRopeLoose = true;
         yield return new WaitForSeconds(_ropeTimeR);
         _ropeSender.DataSend("S\n");
+        _isRopeLoose = false;
     }
 
     public IEnumerator DisappearShock() // 鷹が飛び立つときの衝撃提示の関数
     {
         _weightSender.DataSend("R\n" + _weightSpeedR.ToString() + "\n");
+        _isWeightUp = true;
         yield return new WaitForSeconds(_weightTimeR);
         _weightSender.DataSend("S\n");
+        _isWeightUp = false;
     }
 
     /*
